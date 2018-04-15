2d3fa6f6df4761e5526df533d8a045ddd3b1c53d
TamLH <tamlh@atmarkcafe.org>
2018-04-15 18:19:13 +0700

    rspec 10 ** 24

--- lib/kansuji.rb
+++ lib/kansuji.rb
@@ -1,8 +1,7 @@
 require 'pry'
-class Integer
+class String
   NUMS_HASH = {1 => "一", 2 => "二", 3 => "三", 4 => "四", 5 => "五", 6 => "六", 7 => "七", 8 => "八", 9 => "九", 10 => "十", 100 => "百", 10 ** 3 => "千", 10 ** 4 => "万"}
   UNIT_HASH = {2 => '万', 3 => "億", 4 => "兆" , 5 => "京", 6 => "垓", 7 => "禾予", 8 => "穣", 9 => "溝", 10 => "澗", 11 => "正", 12 => "載", 13 => "極", 14 => "恒河沙", 15 => "阿僧祇"}
-
   def unit_hash(number)
     return NUMS_HASH[10]if number >= 10 && number < 100
     return NUMS_HASH[100] if number >= 100 && number < 1000
@@ -12,46 +11,32 @@ class Integer
 
   def group_f
     result = []
-    inputs = self
-    arr_input = self.to_s.split('')
-    arr_input.each_with_index do |val, index|
-      next if val.to_i == 0
+    inputs = self.to_i
+    arr_input = inputs.to_s.split('')
+    arr_input.each_with_index do |val, index| # puts val
+      next inputs = inputs/10 if val.to_i == 0
       result << NUMS_HASH[val.to_i] if val.to_i != 1
       result << unit_hash(inputs) if val.to_i != 1
       result << unit_hash(inputs) if val.to_i == 1
-      result << NUMS_HASH[val.to_i] if  val.to_i == 1 && index == arr_input.size - 1
-      inputs = inputs/10
-      # binding.pry
+      result << NUMS_HASH[val.to_i] if val.to_i == 1 && index == arr_input.size - 1
+      inputs = inputs/10 # puts inputs # binding.pry
     end
     string = result.join("")
-    return string
   end
-
-def to_kansuji
-  new_input_array = self.to_s.reverse.split('').each_slice(4)
-
-  rs = []
-  reverse_array = new_input_array.reverse_each
-  arr_size = reverse_array.size
-  puts arr_size
-
-  reverse_array.each do | arrays |
-
-    print "\narray: "
-    print arrays.reverse.join()
-    number = arrays.reverse.join().to_i
-    print "\nnumber: "
-    print number
-    print "\n"
-    rs << number.group_f
-    rs << UNIT_HASH[arr_size] unless number == 0
-    arr_size -= 1
+  def to_kansuji
+    rs = []
+    reverse_array = self.to_s.reverse.split('').each_slice(4).reverse_each
+    arr_size = reverse_array.size # print 'arr_size: ' + arr_size.to_s + "\n"
+    reverse_array.each do | arrays |      # print arrays      # print "\n"
+      number = arrays.reverse.join()      # print number.to_s + "\n"
+      next arr_size -= 1 if number.to_i == 0
+      rs << number.group_f
+      rs << UNIT_HASH[arr_size] unless number == 0
+      arr_size -= 1
+    end    # puts string = rs.join    return rs.join
   end
-  puts rs.join
-end
-  # # #
 end
 
-puts 'Enter number to kanji'
-input = gets.chomp
-puts input.to_s.to_i.to_kansuji
+# puts 'Enter number to kanji'
+# input = gets.chomp
+# puts input.to_kansuji
---
+++ spec/lib/kansuji_spec.rb
@@ -0,0 +1,350 @@
+require 'rails_helper'
+require 'kansuji'
+
+describe 'Kansuji' do
+  context '10 ** 1 ' do
+    it '1' do
+      '1'.to_kansuji.should == '一'
+    end
+    it '01' do
+      '01'.to_kansuji.should == '一'
+    end
+    it '10' do
+      '10'.to_kansuji.should == '十'
+    end
+    it '11' do
+      '11'.to_kansuji.should == '十一'
+    end
+    it '23' do
+      '23'.to_kansuji.should == '二十三'
+    end
+  end
+  context '10 ** 2 ' do
+    it '010' do
+      '010'.to_kansuji.should == '十'
+    end
+    it '100' do
+      '100'.to_kansuji.should == '百'
+    end
+    it '101' do
+      '101'.to_kansuji.should == '百一'
+    end
+    it '234' do
+      '234'.to_kansuji.should == '二百三十四'
+    end
+  end
+  context '10 ** 3 ' do
+    it '1000' do
+      '1000'.to_kansuji.should == '千'
+    end
+    it '1001' do
+      '1001'.to_kansuji.should == '千一'
+    end
+    it '1234' do
+      '1234'.to_kansuji.should == '千二百三十四'
+    end
+  end
+  context '10 ** 4 ' do
+    it '10000' do
+      '10000'.to_kansuji.should == '一万'
+    end
+    it '10001' do
+      '10001'.to_kansuji.should == '一万一'
+    end
+    it '51234' do
+      '51234'.to_kansuji.should == '五万千二百三十四'
+    end
+  end
+  context '10 ** 5 ' do
+    it '100000' do
+      '100000'.to_kansuji.should == '十万'
+    end
+    it '100001' do
+      '100001'.to_kansuji.should == '十万一'
+    end
+    it '651234' do
+      '651234'.to_kansuji.should == '六十五万千二百三十四'
+    end
+  end
+  context '10 ** 6 ' do
+    it '1000000' do
+      '1000000'.to_kansuji.should == '百万'
+    end
+    it '01000000' do
+      '01000000'.to_kansuji.should == '百万'
+    end
+    it '1000001' do
+      '1000001'.to_kansuji.should == '百万一'
+    end
+    it '7651234' do
+      '7651234'.to_kansuji.should == '七百六十五万千二百三十四'
+    end
+  end
+  context '10 ** 7 ' do
+    it '10000000' do
+      '10000000'.to_kansuji.should == '千万'
+    end
+    it '010000000' do
+      '010000000'.to_kansuji.should == '千万'
+    end
+    it '10000001' do
+      '10000001'.to_kansuji.should == '千万一'
+    end
+    it '87651234' do
+      '87651234'.to_kansuji.should == '八千七百六十五万千二百三十四'
+    end
+  end
+  context '10 ** 8 ' do
+    it '100000000' do
+      '100000000'.to_kansuji.should == '一億'
+    end
+    it '0100000000' do
+      '0100000000'.to_kansuji.should == '一億'
+    end
+    it '100000001' do
+      '100000001'.to_kansuji.should == '一億一'
+    end
+    it '987651234' do
+      '987651234'.to_kansuji.should == '九億八千七百六十五万千二百三十四'
+    end
+  end
+  context '10 ** 9 ' do
+    it '1000000000' do
+      '1000000000'.to_kansuji.should == '十億'
+    end
+    it '01000000000' do
+      '01000000000'.to_kansuji.should == '十億'
+    end
+    it '1000000001' do
+      '1000000001'.to_kansuji.should == '十億一'
+    end
+    it '1987651234' do
+      '1987651234'.to_kansuji.should == '十九億八千七百六十五万千二百三十四'
+    end
+  end
+  context '10 ** 10 ' do
+    it '10000000000' do
+      '10000000000'.to_kansuji.should == '百億'
+    end
+    it '010000000000' do
+      '010000000000'.to_kansuji.should == '百億'
+    end
+    it '10000000001' do
+      '10000000001'.to_kansuji.should == '百億一'
+    end
+    it '10987651234' do
+      '10987651234'.to_kansuji.should == '百九億八千七百六十五万千二百三十四'
+    end
+  end
+  context '10 ** 11 ' do
+    it '100000000000' do
+      '100000000000'.to_kansuji.should == '千億'
+    end
+    it '0100000000000' do
+      '0100000000000'.to_kansuji.should == '千億'
+    end
+    it '100000000001' do
+      '100000000001'.to_kansuji.should == '千億一'
+    end
+    it '109876512340' do
+      '109876512340'.to_kansuji.should == '千九十八億七千六百五十一万二千三百四十'
+    end
+  end
+  context '10 ** 12 ' do
+    it '1000000000000' do
+      '1000000000000'.to_kansuji.should == '一兆'
+    end
+    it '01000000000000' do
+      '01000000000000'.to_kansuji.should == '一兆'
+    end
+    it '1000000000001' do
+      '1000000000001'.to_kansuji.should == '一兆一'
+    end
+    it '1200000000001' do
+      '1200000000001'.to_kansuji.should == '一兆二千億一'
+    end
+    it '1098706512340' do
+      '1098706512340'.to_kansuji.should == '一兆九百八十七億六百五十一万二千三百四十'
+    end
+  end
+  context '10 ** 13 ' do
+    it '10000000000000' do
+      '10000000000000'.to_kansuji.should == '十兆'
+    end
+    it '010000000000000' do
+      '010000000000000'.to_kansuji.should == '十兆'
+    end
+    it '10000000000001' do
+      '10000000000001'.to_kansuji.should == '十兆一'
+    end
+    it '10985706512340' do
+      '10985706512340'.to_kansuji.should == '十兆九千八百五十七億六百五十一万二千三百四十'
+    end
+  end
+  context '10 ** 14 ' do
+    it '100000000000000' do
+      '100000000000000'.to_kansuji.should == '百兆'
+    end
+    it '0100000000000000' do
+      '0100000000000000'.to_kansuji.should == '百兆'
+    end
+    it '100000000000001' do
+      '100000000000001'.to_kansuji.should == '百兆一'
+    end
+    it '109857806512340' do
+      '109857806512340'.to_kansuji.should == '百九兆八千五百七十八億六百五十一万二千三百四十'
+    end
+  end
+  context '10 ** 15 ' do
+    it '1000000000000000' do
+      '1000000000000000'.to_kansuji.should == '千兆'
+    end
+    it '01000000000000000' do
+      '01000000000000000'.to_kansuji.should == '千兆'
+    end
+    it '1000000000000001' do
+      '1000000000000001'.to_kansuji.should == '千兆一'
+    end
+    it '9000000003000001' do
+      '9000000003000001'.to_kansuji.should == '九千兆三百万一'
+    end
+    it '1098578069512340' do
+      '1098578069512340'.to_kansuji.should == '千九十八兆五千七百八十億六千九百五十一万二千三百四十'
+    end
+  end
+  context '10 ** 16 ' do
+    it '10000000000000000' do
+      '10000000000000000'.to_kansuji.should == '一京'
+    end
+    it '010000000000000000' do
+      '010000000000000000'.to_kansuji.should == '一京'
+    end
+    it '10000000000000001' do
+      '10000000000000001'.to_kansuji.should == '一京一'
+    end
+    it '10985780695123040' do
+      '10985780695123040'.to_kansuji.should == '一京九百八十五兆七千八百六億九千五百十二万三千四十'
+    end
+  end
+  context '10 ** 17 ' do
+    it '100000000000000000' do
+      '100000000000000000'.to_kansuji.should == '十京'
+    end
+    it '0100000000000000000' do
+      '0100000000000000000'.to_kansuji.should == '十京'
+    end
+    it '100000000000000001' do
+      '100000000000000001'.to_kansuji.should == '十京一'
+    end
+    it '109857806951230409' do
+      '109857806951230409'.to_kansuji.should == '十京九千八百五十七兆八千六十九億五千百二十三万四百九'
+    end
+  end
+  context '10 ** 18 ' do
+    it '1000000000000000000' do
+      '1000000000000000000'.to_kansuji.should == '百京'
+    end
+    it '01000000000000000000' do
+      '01000000000000000000'.to_kansuji.should == '百京'
+    end
+    it '1000000000000000001' do
+      '1000000000000000001'.to_kansuji.should == '百京一'
+    end
+    it '1092857806951230409' do
+      '1092857806951230409'.to_kansuji.should == '百九京二千八百五十七兆八千六十九億五千百二十三万四百九'
+    end
+  end
+  context '10 ** 19 ' do
+    it '10000000000000000000' do
+      '10000000000000000000'.to_kansuji.should == '千京'
+    end
+    it '010000000000000000000' do
+      '010000000000000000000'.to_kansuji.should == '千京'
+    end
+    it '10000000000000000001' do
+      '10000000000000000001'.to_kansuji.should == '千京一'
+    end
+    it '10925857806951230409' do
+      '10925857806951230409'.to_kansuji.should == '千九十二京五千八百五十七兆八千六十九億五千百二十三万四百九'
+    end
+  end
+  context '10 ** 20 ' do
+    it '100000000000000000000' do
+      '100000000000000000000'.to_kansuji.should == '一垓'
+    end
+    it '0100000000000000000000' do
+      '0100000000000000000000'.to_kansuji.should == '一垓'
+    end
+    it '100000000000000000001' do
+      '100000000000000000001'.to_kansuji.should == '一垓一'
+    end
+    it '100000000000008000001' do
+      '100000000000008000001'.to_kansuji.should == '一垓八百万一'
+    end
+    it '109258578069512304095' do
+      '109258578069512304095'.to_kansuji.should == '一垓九百二十五京八千五百七十八兆六百九十五億千二百三十万四千九十五'
+    end
+  end
+  context '10 ** 21 ' do
+    it '1000000000000000000000' do
+      '1000000000000000000000'.to_kansuji.should == '十垓'
+    end
+    it '01000000000000000000000' do
+      '01000000000000000000000'.to_kansuji.should == '十垓'
+    end
+    it '1000000000000000000001' do
+      '1000000000000000000001'.to_kansuji.should == '十垓一'
+    end
+    it '1092585780695123040956' do
+      '1092585780695123040956'.to_kansuji.should == '十垓九千二百五十八京五千七百八十兆六千九百五十一億二千三百四万九百五十六'
+    end
+  end
+  context '10 ** 22 ' do
+    it '10000000000000000000000' do
+      '10000000000000000000000'.to_kansuji.should == '百垓'
+    end
+    it '010000000000000000000000' do
+      '010000000000000000000000'.to_kansuji.should == '百垓'
+    end
+    it '10000000000000000000001' do
+      '10000000000000000000001'.to_kansuji.should == '百垓一'
+    end
+    it '10925857806951230409546' do
+      '10925857806951230409546'.to_kansuji.should == '百九垓二千五百八十五京七千八百六兆九千五百十二億三千四十万九千五百四十六'
+    end
+  end
+  context '10 ** 23 ' do
+    it '100000000000000000000000' do
+      '100000000000000000000000'.to_kansuji.should == '千垓'
+    end
+    it '0100000000000000000000000' do
+      '0100000000000000000000000'.to_kansuji.should == '千垓'
+    end
+    it '100000000000000000000001' do
+      '100000000000000000000001'.to_kansuji.should == '千垓一'
+    end
+    it '50925857806951230409546' do
+      '50925857806951230409546'.to_kansuji.should == '五百九垓二千五百八十五京七千八百六兆九千五百十二億三千四十万九千五百四十六'
+    end
+  end
+  context '10 ** 24 ' do
+    it '1000000000000000000000000' do
+      '1000000000000000000000000'.to_kansuji.should == '一禾予'
+    end
+    it '01000000000000000000000000' do
+      '01000000000000000000000000'.to_kansuji.should == '一禾予'
+    end
+    it '1000000000000000000000001' do
+      '1000000000000000000000001'.to_kansuji.should == '一禾予一'
+    end
+    it '1000000000000100000001001' do
+      '1000000000000100000001001'.to_kansuji.should == '一禾予千億千一'
+    end
+    it '4000006000000000000000000' do
+      '4000006000000000000000000'.to_kansuji.should == '四禾予六百京'
+    end
+    it '5095857806951230410954699' do
+      '5095857806951230410954699'.to_kansuji.should == '五禾予九百五十八垓五千七百八十京六千九百五十一兆二千三百四億千九十五万四千六百九十九'
+    end
+  end
+end
