1aaaa0019ac8a9ad1d920c243133e13e7b3f4f95
TamLH <tamlh@atmarkcafe.org>
2018-04-18 20:56:30 +0700

    fix link

--- app/controllers/to_kansujis_controller.rb
+++ app/controllers/to_kansujis_controller.rb
@@ -2,13 +2,10 @@ class ToKansujisController < ApplicationController
   def index
     re = /\d/
     str = params[:input] ? params[:input] : '0'
-
     @input = []
     @input = @input.join("").to_i if str.scan(re) { |match| @input << match.to_s}
-    @exp = "___#{'%e' %@input} "
 
+    # @exp = "___#{'%e' %@input} "
     @result = @input.to_kansuji
-
-    puts @input
   end
 end
--- app/controllers/to_numbers_controller.rb
+++ app/controllers/to_numbers_controller.rb
@@ -3,10 +3,11 @@ class ToNumbersController < ApplicationController
   def index
     re = /零|一|二|三|四|五|六|七|八|九|十|百|千|万|万|億|兆|京|垓|𥝱|穣|溝|澗|正|載|極|恒河沙|阿僧祇|那由他|不可思議|無量大数/i
     str = params[:input] ? params[:input] : '零'
-    @input = []
+    @input = ['']
     @input = @input.join("") if str.scan(re) { |match| @input << match.to_s}
 
     puts "input #{@input} ======="
     @result = @input.to_number
+    puts @result
   end
 end
