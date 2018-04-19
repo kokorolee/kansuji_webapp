879fb9e6933c6ac4ca0102e9591fb280da99ca37
TamLH <tamlh@atmarkcafe.org>
2018-04-19 17:14:06 +0700

    refactor qui-algirothms

--- lib/kansuji-qui.rb
+++ lib/kansuji-qui.rb
@@ -1,64 +1,46 @@
-require 'datatables'
+KAN = { 0=>"零", 1=>"一", 2=>"二", 3=>"三", 4=>"四", 5=>"五", 6=>"六", 7=>"七",
+      8=>"八", 9=>"九", 10=>"十", 100=>"百", 1000=>"千", 10000=>"万", (10**8)=>"億",
+      (10**12)=>"兆", (10**16)=>"京", (10**20)=>"垓", (10**24)=>"𥝱",
+      (10**28)=>"穣", (10**32)=>"溝", (10**36)=>"澗", (10**40)=>"正",
+      (10**44)=>"載", (10**48)=>"極", (10**52)=>"恒河沙", (10**56)=>"阿僧祇",
+      (10**60)=>"那由他", (10**64)=>"不可思議", (10**68)=>"無量大数" }
+ROM = KAN.to_a[13..30]
+
 class String
   def to_number
     result = 0
-    return get_number(self) if !KEY.any?{|key| self.include?(key)}
-    hash_kanji.each do |array|
-      return result += get_number(self[array[:begin]..(array[:end])]).to_i  if hash_kanji.last == array
-      if array[:end] - array[:begin] == 0
-        result += ROM_TO_KANJI[array[:key]].to_i
-      else
-        result += get_number(self[array[:begin]..(array[:end]-1)]).to_i * ROM_TO_KANJI[array[:key]].to_i # >0
-      end
+    current = 0
+    return number(self) if ROM.empty?{ |a| self.include?(a[1]) }
+    ROM.reverse_each do |array|
+      next if self.index(array[1]).nil?
+      result += number(self[current..self.index(array[1])-1]) * KAN.key(array[1])
+      current = self.index(array[1]) + array[1].length
     end
+    return result + number(self[current..self.length]).to_i
   end
-  def get_number(string)
-    result = 0
-    while string.present? do
-      current = ROM_TO_KANJI[string[0]].to_i
-      if ROM_TO_KANJI[string[1]].to_i % 10 == 0 && string[1].present? && current % 10 != 0 #eample: 20,200,2020
-        current = current*ROM_TO_KANJI[string[1]].to_i
-        string.slice!(0)
-      end
-      result += current.to_i
-      return result if string.length == 1
-      string.slice!(0)
-    end
-  end
-  def hash_kanji
-    array = []
-    KEY.reverse.each do |item|
-      array.push(begin: 0, end: self.index(item), key: item) if array.blank? && self.index(item).present?
-      array.push(begin: (array.last[:end]+array.last[:key].length), end: self.index(item), key: item) if array.present? && self.index(item).present? && (array.last[:end]+1) <= self.index(item)
-    end
-    return array.push(begin: (array.last[:end]+array.last[:key].length), end: self.length, key:'')
+
+  def number str
+    current = KAN.key(str.slice!(0)).to_i
+    return current if str.length == 0 || str.nil?
+    return ((current / 10 == 0 && KAN.key(str[0]).to_i % 10 == 0) ? \
+                        current * KAN.key(str.slice!(0)) : current) + number(str)
   end
 end
+
 class Numeric
-  def to_kansuji()
-    return (kanji_to_demo(self.to_s)) if self.to_s.length <= 5
-    result = ''
-    self.to_s.reverse.split('').each_slice(4).each.with_index do |fa, index|
-      key = index > 0 ? KEY[index-1] : '' if fa.reverse.join('').to_i > 0 # defined?(result).nil? ? result=(kanji_to_demo(fa.reverse.join('')).to_s + key.to_s) : result=(kanji_to_demo(fa.reverse.join('')).to_s + key.to_s + result.to_s)
-      result=(kanji_to_demo(fa.reverse.join('')).to_s + key.to_s + result.to_s)
+  def to_kansuji
+    self == 0 ? (return KAN[self]) : result = ''
+    self.to_i.to_s.reverse.split('').each_slice(4).with_index().each do |array, index|
+      key = index > 0 ? KAN.to_a[index+12][1] : '' if array.reverse.join('').to_i > 0
+      result = kanji(array.reverse.join('')) + key.to_s + result.to_s
+      return result if index == self.to_s.reverse.split('').each_slice(4).size - 1
     end
-    return result
   end
-  def kanji_to_demo(string)
-    kan = ''
-    while string do
-      if string[0] != '0'
-        return (kan = kan.to_s + KANA_TO_ROM[string[0].to_i]) if string.length == 1 #why a.to_s ma @kan='' but @kan = nil
-        if string[0] == '1' && string.length >= 5 # duoi 10000 k co 一
-          kan = kan.to_s + '一'.to_s + KANA_TO_ROM[(string[0].to_i*10**(string.length-1))].to_s #xu ly so 1
-        elsif string[0] == '1' #tren 10000 co so 一
-          kan = kan.to_s + KANA_TO_ROM[(string[0].to_i*10**(string.length-1))].to_s  #xu ly so 1
-        else
-          kan = kan.to_s + KANA_TO_ROM[string[0].to_i].to_s + KANA_TO_ROM[(10**(string.length-1))].to_s
-        end
-      end
-      string.slice!(0)
-      return kan if string.length == 0
-    end
+
+  def kanji str
+    return str[0] == '0' ? '' : KAN[str[0].to_i] if str.length == 1
+    return kanji(str[1..str.length]) if str[0] == '0'
+    return (str[0] == '1' ? KAN[str.slice!(0).to_i * 10**str.length] : \
+            KAN[str.slice!(0).to_i] + KAN[10**str.length]) +  kanji(str)
   end
 end
